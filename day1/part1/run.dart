c28fe5681e0eb49b9b8f231e9796023b99e152b8
Jeff Martin <jmartin127@gmail.com>
2022-11-30 21:50:04 -0700

    relative path for reading from input file

--- day1/part1/run.dart
+++ day1/part1/run.dart
@@ -1,11 +1,10 @@
 import '../../util/util.dart';
 
 Future<void> main() async {
-  final lines = await Util.readFileAsInts(
-      '/Users/jeff/workspace/advent-of-code-2022/day1/input.txt');
-
+  final lines = await Util.readFileAsInts('input.txt');
   var total = 0;
-  for (final line in lines) {
+  for (int i = 0; i < lines.length; i++) {
+    var line = lines[i];
     total += line;
   }
   print(total);

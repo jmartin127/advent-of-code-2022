70552978bd95d42f9faed7d8635351c77f5b0c80
Jeff Martin <jmartin127@gmail.com>
2022-12-22 22:18:16 -0700

    Laying out algorithm

--- lib/day19/part1/run.dart
+++ lib/day19/part1/run.dart
@@ -69,4 +69,31 @@ Future<void> main() async {
   for (final blueprint in blueprints) {
     print(blueprint);
   }
+
+  // Fortunately, you have exactly one ore-collecting robot in your pack that
+  // you can use to kickstart the whole operation.
+
+  // Each robot can collect 1 of its resource type per minute.
+
+  // It also takes one minute for the robot factory to construct any type of
+  // robot, although it consumes the necessary resources available when construction begins.
+
+  // In analyzing how the data works... it seems the algorithm needs to be:
+  // 1. Build a geode robot if possible (requires obsidian and ore)
+  // 2. Build an obsidian robot if possibe (requres clay and ore)
+  //
+  // Decide if we should build a clay robot or an ore robot? Explore both paths?
+  // 3. Build a clay robot if possible (requires ore)
+  // 4. Build an ore robot (requires ore)
+  //
+  // Questions:
+  // How do you decide if you should build an ore robot or a clay robot?
+  // How do you know when you should "save up" enough ore to get a higher-level robot vs. spend it on an ore or clay robot?
+  // ... If you have enough obsidian, for example, then seems you should not spend ore on other things
+
+  for (final blueprint in blueprints) {
+    Map<Resource, int> resources = {};
+    Map<Robot, int> numRobotsByType = {};
+    for (int minute = 1; minute <= 24; minute++) {}
+  }
 }

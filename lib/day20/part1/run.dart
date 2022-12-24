cf5814a454a41b77861b39cf85c782c693ea6947
Jeff Martin <jmartin127@gmail.com>
2022-12-23 23:07:44 -0700

    day 20 part 1, seems right, but answer on real dataset isn't matching

--- lib/day20/part1/run.dart
+++ lib/day20/part1/run.dart
@@ -43,22 +43,31 @@ Future<void> main() async {
       zeroElement = numbers.last;
     }
   }
-  print('Length: ${numbers.length}');
+  // print('Length: ${numbers.length}');
 
   // The numbers should be moved in the order they originally appear in the
   // encrypted file. Numbers moving around during the mixing process do not
   // change the order in which the numbers are moved.
+  int count = 0;
   for (final currentNum in numbersOriginal) {
-    print('Moving $currentNum');
+    count++;
+    if (count % 100 == 0) {
+      print('Count: $count');
+    }
+    // print('Moving $currentNum');
 
-    final index = findIndexOfElement(numbers, currentNum);
+    final index = findIndexOfElement(numbers, currentNum)!;
     final foundNum = numbers.elementAt(index);
-    print('\tFound at index: $index');
+    // print('\tFound at index: $index');
 
     if (foundNum.value > 0) {
       // determine where the inseration is going to happen
       final insertAfterIndex = index + foundNum.value;
       final adjustedIndex = insertAfterIndex % numbersOriginal.length;
+      if (adjustedIndex == index) {
+        // nothing to do, same spot
+        continue;
+      }
 
       // if it is wanting to move to length-1, then move it to the beginning
       if (adjustedIndex == numbers.length - 1) {
@@ -77,15 +86,18 @@ Future<void> main() async {
       if (adjustedIndex < 0) {
         adjustedIndex = numbersOriginal.length + adjustedIndex;
       }
+      if (adjustedIndex == index) {
+        // nothing to do, same spot
+        continue;
+      }
 
       // if it is wanting to move to 0, then move it to the end
       if (adjustedIndex == 0) {
         numbers.remove(foundNum);
         numbers.add(foundNum);
       } else {
-        final subsequentElement = numbers.elementAt(adjustedIndex);
-
         // remove and add in new location
+        final subsequentElement = numbers.elementAt(adjustedIndex);
         numbers.remove(foundNum);
         subsequentElement.insertBefore(foundNum);
       }
@@ -93,36 +105,31 @@ Future<void> main() async {
       // nothing to do if the value is zero
     }
 
-    print('Updated list: $numbers');
-
-    // Then, the grove coordinates can be found by looking at the 1000th,
-    // 2000th, and 3000th numbers after the value 0, wrapping around the list as
-    // necessary.
-    final zeroIndex = findIndexOfElement(numbers, zeroElement!);
-    print('Zero index: $zeroIndex');
-    final one_thousandth =
-        numbers.elementAt((zeroIndex + 1000) % numbers.length);
-    final two_thousandth =
-        numbers.elementAt((zeroIndex + 2000) % numbers.length);
-    final three_thousandth =
-        numbers.elementAt((zeroIndex + 3000) % numbers.length);
-    print(one_thousandth);
-    print(two_thousandth);
-    print(three_thousandth);
-    final sum =
-        one_thousandth.value + two_thousandth.value + three_thousandth.value;
-    print(sum);
+    // print('Updated list: $numbers');
   }
+
+  // Then, the grove coordinates can be found by looking at the 1000th,
+  // 2000th, and 3000th numbers after the value 0, wrapping around the list as
+  // necessary.
+  final zeroIndex = findIndexOfElement(numbers, zeroElement!)!;
+  // print('Zero index: $zeroIndex');
+  final oneThousandth = numbers.elementAt((zeroIndex + 1000) % numbers.length);
+  final twoThousandth = numbers.elementAt((zeroIndex + 2000) % numbers.length);
+  final threeThousandth =
+      numbers.elementAt((zeroIndex + 3000) % numbers.length);
+  final sum = oneThousandth.value + twoThousandth.value + threeThousandth.value;
+  print('Sum: $sum');
+
+  // too high: 13343
 }
 
-int findIndexOfElement(LinkedList<Element> numbers, Element element) {
-  int? foundAtIndex;
-  for (int i = 0; i < numbers.length; i++) {
-    final otherNum = numbers.elementAt(i);
+int? findIndexOfElement(LinkedList<Element> numbers, Element element) {
+  int index = 0;
+  for (final otherNum in numbers) {
     if (element == otherNum) {
-      foundAtIndex = i;
-      break;
+      return index;
     }
+    index++;
   }
-  return foundAtIndex!;
+  return null;
 }
